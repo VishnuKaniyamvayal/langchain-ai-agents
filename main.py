ceb6e41668c924f6e7578973be4dab7593922c15
vishnu <vishnujishnu0987@gmail.com>
2025-04-25 22:52:59 +0530

    agent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # agents
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.agents import load_tools, initialize_agent, AgentType
+from langchain.agents import initialize_agent, AgentType
+from langchain_community.tools import WikipediaQueryRun
+from langchain_community.utilities import WikipediaAPIWrapper
 from dotenv import load_dotenv
 # create a llm
 
@@ -9,15 +11,16 @@ load_dotenv()
 
 # create a llm
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash",temperature=0.6)
-
+wiki = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
 # setup agents
-tools = load_tools(["wikipedia","llm-math"],llm=llm)
+tools = [wiki]
 
 agent = initialize_agent(
     tools,
     llm,
     agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-    verbose=True
 )
 
-agent.run("How much films did irrfan khan acted, add 6 to it")
+response = agent.invoke("when was irrfan khan born") 
+print("Loading")
+print(response["output"])
